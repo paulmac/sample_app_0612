16871c540383150eccffabaef7918ca0f84b087c
paulmac <paul@laserandcosmetic.com>
2012-06-07 16:33:29 +1000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleApp0612::Application.config.secret_token = '6419380ad75c4ade869ebc63558df85acd34f15e90cc40538d0b25df5f2b85d15bf328e81102ffbfdc1b9ccc62903246fe8ef35e0756d86944e4f99724e4a2bc'

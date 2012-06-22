d2d15081ddd7b62abbe9e48349fc5cb7daa0d78e
paulmac <paul@laserandcosmetic.com>
2012-06-22 13:33:50 +1000

    Finish sign in

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -2,8 +2,10 @@ module UsersHelper
   # Returns the Gravatar (http://gravatar.com/) for the given user.
   def gravatar_for(user)
     gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
-    size = options[:size]
-    gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}?s=#{size}"
+# Stopped woking in Section 8.25
+#    size = options[:size]
+#    gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}?s=#{size}"
+    gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}"
     image_tag(gravatar_url, alt: user.name, class: "gravatar")
   end
 end
--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -27,8 +27,8 @@ describe "User pages" do
 
     describe "with valid information" do
       before do
-        fill_in "Name",         with: "Example User1"
-        fill_in "Email",        with: "user1@example.com"
+        fill_in "Name",         with: "Example User"
+        fill_in "Email",        with: "user@example.com"
         fill_in "Password",     with: "foobar"
         fill_in "Confirmation", with: "foobar"
       end
@@ -42,15 +42,16 @@ describe "User pages" do
 
       end
 
-#      describe "after saving the user" do
-#        before { click_button submit }
-#        let(:user) { User.find_by_email('user@example.com') }
-#
-#        it { should have_selector('title', text: user.name) }
-#        it { should have_selector('div.alert.alert-success', text: 'Welcome') }
-#      end
+# uncommented in Sec 8.26
+      describe "after saving the user" do
+        before { click_button submit }
+        let(:user) { User.find_by_email('user@example.com') }
 
+        it { should have_selector('title', text: user.name) }
+        it { should have_selector('div.alert.alert-success', text: 'Welcome') }
+
+        it { should have_link('Sign out') }
+      end
     end
-  end
-  
+  end  
 end

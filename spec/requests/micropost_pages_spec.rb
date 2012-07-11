7ef753720ddc64ce70e6bc41e1edc30b26cfe077
paulmac <paul@laserandcosmetic.com>
2012-07-11 10:44:47 +1000

    Exercises chap10

--- spec/requests/micropost_pages_spec.rb
+++ spec/requests/micropost_pages_spec.rb
@@ -5,9 +5,11 @@ describe "Micropost pages" do
   subject { page }
 
   let(:user) { FactoryGirl.create(:user) }
+
   before { sign_in user }
 
   describe "micropost creation" do
+
     before { visit root_path }
 
     describe "with invalid information" do
@@ -33,6 +35,7 @@ describe "Micropost pages" do
   end # "micropost creation"
 
   describe "micropost destruction" do
+
     before { FactoryGirl.create(:micropost, user: user) }
 
     describe "as correct user" do
@@ -42,6 +45,6 @@ describe "Micropost pages" do
         expect { click_link "delete" }.should change(Micropost, :count).by(-1)
       end
     end
-  end
+  end # "micropost destruction"
 
 end
